5bbcb0f00f4d333ff36aafbceb05c33ad4b6e13d
ajaj123-debug <ajaj42699@gmail.com>
2024-09-18 23:30:26 +0530

    change in settings/

--- ocr_project_pr1/ocr_project_pr1/settings.py
+++ ocr_project_pr1/ocr_project_pr1/settings.py
@@ -131,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+import os
+
+# Path where uploaded media files are stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'

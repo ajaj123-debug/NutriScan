f43f085c5181a9d8364ec80f10586dc8165eeaca
ajaj123-debug <ajaj42699@gmail.com>
2024-09-18 22:57:09 +0530

    change in model

--- ocr_project_pr1/ocr_app_pr1/models.py
+++ ocr_project_pr1/ocr_app_pr1/models.py
@@ -11,3 +11,31 @@ class HarmfulIngredient(models.Model):
 
     def __str__(self):
         return self.name
+    class meta:
+        db_table = "HarmfulIngredient"
+
+
+
+from django.db import models
+
+# Create your models here.
+
+class Harmful_Ingredients(models.Model):
+    HARM_LEVEL_CHOICES = {
+        'select': 'Select',
+        'low': 'Low',
+        'medium': 'Medium',
+        'high': 'High',
+    }
+    ingredient= models.CharField(max_length = 150)
+    desc = models.TextField()
+    Harm_Level= models.CharField(max_length=10, choices=HARM_LEVEL_CHOICES, default= 'select')
+    class Meta:
+        db_table = 'Harmful_Ingredients'
+
+class product_type(models.Model):
+    product_name = models.CharField(max_length = 150)
+    product_image = models.ImageField(upload_to='products/')
+    product_link = models.URLField()
+    class Meta:
+        db_table = 'product_type'
